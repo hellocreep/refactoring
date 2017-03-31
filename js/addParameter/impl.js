f4ccb575ff537c30c61d927c1a1ac0969b39e7f5
hellocreep <hellocreep@gmail.com>
2017-03-31 20:59:33 +0800

    [Add Parameter] add a new function with new parameters

--- js/addParameter/impl.js
+++ js/addParameter/impl.js
@@ -2,4 +2,8 @@ function greet(name) {
   return `Hello, ${name}!`
 }
 
+function greetFn(firstPersonName, secondPersonName) {
+  return `Hello, ${firstPersonName} and ${secondPersonName}!`
+}
+
 module.exports = greet
