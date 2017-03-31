c8b95f1f18ebe638d8fb5c1351036f89f5e8c661
Zhen Tang <cloudztang@gmail.com>
2017-03-31 23:45:27 +0800

    [Zhen] [changeDirectional] add steps.

--- src/main/com/thoughtworks/refactoring/ChangeDirectional.java
+++ src/main/com/thoughtworks/refactoring/ChangeDirectional.java
@@ -17,6 +17,14 @@ public class ChangeDirectional {
         orders.add(newOrderWithCustomer("John"));
 
         // I want get the order count of John
+
+        // 1. Add a field for the back pointer.
+        // 2. Decide which class will control the association.
+        // 3. Create a helper method on the noncontrolling side of the association.
+        //    Name this method to clearly indicate its restricted use.
+        // 4. If the existing modifier is on the controlling side, modify it to update the back points.
+        // 5. If the existing modifier is on the controlled side, create a controlling
+        //    method on the controlling side to call it from the existing modifier.
     }
 
     private Order newOrderWithCustomer(String name) {
