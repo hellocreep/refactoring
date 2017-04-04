97e757b8569cae043e166c3118ba6056365f6dd4
hellocreep <hellocreep@gmail.com>
2017-04-04 20:57:56 +0800

    [Change Value to Reference] - preload customer and store them in a map

--- js/changeValueToReference/customer.js
+++ js/changeValueToReference/customer.js
@@ -1,6 +1,18 @@
+let _instances = new Map
+
 class Customer {
   static create(name) {
     return new Customer(name)
+  }
+
+  static loadCustomers() {
+    new Customer('Horace').store()
+    new Customer('Pete').store()
+    new Customer('Syvia').store()
+  }
+
+  store() {
+    _instances.set(this.getName(), this)
   }
 
   constructor(name) {
