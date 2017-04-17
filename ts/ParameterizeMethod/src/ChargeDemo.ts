b4b1b869c10a48c1bc775e9a233c386de4311ba6
Evan <haoer715@gmail.com>
2017-04-17 13:50:46 +0800

    [ Hao ] [ParameterizeMethod] - extract condition.

--- ts/ParameterizeMethod/src/ChargeDemo.ts
+++ ts/ParameterizeMethod/src/ChargeDemo.ts
@@ -1,18 +1,41 @@
 import Dollars from "./Dollars";
 
+const IntegerMax = 999999999;
+
 class Charge {
     protected baseCharge(): Dollars {
-        let result: number = Math.min(this.lastUsage(), 100) * 0.03;
+        let result: number = this.chargeLessThan100();
 
         if (this.lastUsage() > 100) {
-            result += (Math.min(this.lastUsage(), 200) - 100) * 0.05;
+            result += this.chargeGreaterThan100LessThan200();
         }
 
         if (this.lastUsage() > 200) {
-            result += (this.lastUsage() - 200) * 0.07;
+            result += this.chargeGreaterThan200();
         }
 
         return new Dollars(result);
+    }
+
+    protected chargeLessThan100(): number {
+        if (!(this.lastUsage() > 0)) {
+            return 0;
+        }
+        return Math.min(this.lastUsage(), 100) * 0.03;
+    }
+
+    protected chargeGreaterThan100LessThan200(): number {
+        if (!(this.lastUsage() > 100)) {
+            return 0;
+        }
+        return (Math.min(this.lastUsage(), 200) - 100) * 0.05;
+    }
+
+    protected chargeGreaterThan200(): number {
+        if (!(this.lastUsage() > 200)) {
+            return 0;
+        }
+        return (this.lastUsage() - 200) * 0.07;
     }
 
     protected lastUsage(): number {
