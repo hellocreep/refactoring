db19eeaf96b37c891a02dc60de81bc92a8a5818c
Evan <haoer715@gmail.com>
2017-04-27 08:28:21 +0800

    [Hao] [Remove Setting Method] - Remove the setting method and make the field final.

--- ts/RemoveSettingMethod/src/example/RemoveSettingMethod.ts
+++ ts/RemoveSettingMethod/src/example/RemoveSettingMethod.ts
@@ -1,13 +1,9 @@
  namespace Example {
    export class Amount {
-        private _id: string;
+        private readonly _id: string;
 
         constructor(id: string) {
             this._id = id;
-        }
-
-        public setId(arg: string) {
-            this._id = arg;
         }
     }
 }
