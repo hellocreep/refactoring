7513d03dcfe187429acfb8727b8e440b9dff2e82
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-05-07 22:29:42 +0800

    [jiefeng] introduceParameterObject: step3.1 - deleting old parameters mini, replacing it with the parameter object SquareCondition.

--- js/introduceParameterObject/__test__/allSquare.test.js
+++ js/introduceParameterObject/__test__/allSquare.test.js
@@ -11,9 +11,9 @@ const squareThird= new Square(5, 'yellow');
 const allSquare = new AllSquare([squareFirst, squareSecond, squareThird]);
 
 test('AllSquare special square area is 0 when no such color', function (t) {
-  t.is(allSquare.getSpecialSquareArea(3, 20, ['grey']), 0, new SquareCondition());
+  t.is(allSquare.getSpecialSquareArea(20, ['grey'], new SquareCondition(3, undefined, [])), 0);
 });
 
 test('AllSquare special square area', function (t) {
-  t.is(allSquare.getSpecialSquareArea(5, 10, ['red']), 9, new SquareCondition());
+  t.is(allSquare.getSpecialSquareArea(10, ['red'], new SquareCondition(5, undefined, [])), 9);
 });
--- js/introduceParameterObject/allSquare.js
+++ js/introduceParameterObject/allSquare.js
@@ -3,10 +3,10 @@ class AllSquare {
     this._allSquare = allSquare;
   };
 
-  getSpecialSquareArea(mini, max, colorList, undefined) {
+  getSpecialSquareArea(max, colorList, squareCondition) {
     var specialArea = 0;
     this._allSquare.map(function (square) {
-      if (square.getArea() >= mini && square.getArea() <= max && colorList.includes(square.getColor())) {
+      if (square.getArea() >= squareCondition.getMiniArea() && square.getArea() <= max && colorList.includes(square.getColor())) {
         specialArea += square.getArea();
       }
     });
