1565ed36d6c4175b175c032afd0e8c788b43b1bb
Evan <haoer715@gmail.com>
2017-05-22 08:58:57 +0800

    [ Pull Down Method ] - copy the body into each subclass.

--- ts/PushDownMethod/Employee.ts
+++ ts/PushDownMethod/Employee.ts
@@ -5,7 +5,13 @@ export default class Employee {
 }
 
 export class Salesman extends Employee {
+    public getQuota() {
+        return 'some data';
+    }
 }
 
 export class Englneer extends Employee {
+    public getQuota() {
+        return 'some data';
+    }
 }
--- ts/PushDownMethod/__test__/Employee.spec.ts
+++ ts/PushDownMethod/__test__/Employee.spec.ts
@@ -1,8 +1,18 @@
-import Employee from './../Employee';
+import Employee, { Salesman, Englneer } from './../Employee';
 
 describe('Pull Down Method', () => {
     it('Employee.getQuota', () => {
         const employee = new Employee();
         expect(employee.getQuota()).toBe('some data');
+    });
+
+    it('Salesman.getQuota', () => {
+        const salesman = new Salesman();
+        expect(salesman.getQuota()).toBe('some data');
+    });
+
+    it('Englneer.getQuota', () => {
+        const englneer = new Englneer();
+        expect(englneer.getQuota()).toBe('some data');
     });
 });
