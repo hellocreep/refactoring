afb7366c9e19292314bc9ce6e8ef123f33c05076
JellyLu <634725546@qq.com>
2017-06-25 23:14:41 +0800

    [Yangjie] - Replace Data Value with Object - Original code.

---
+++ src/main/com/thoughtworks/refactoring/replaceDataValueWithObject/Order.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.refactoring.replaceDataValueWithObject;
+
+public class Order {
+    public String customer;
+
+
+    public Order(String customerName) {
+        this.customer = customerName;
+    }
+}
---
+++ src/test/com/thoughtworks/refactoring/replaceDataValueWithObject/OrderTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.refactoring.replaceDataValueWithObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrderTest {
+
+    @Test
+    public void getOrderOwnerName() throws Exception {
+        Order order = new Order("LU");
+
+        String customerName = order.customer;
+        assertEquals(customerName, "LU");
+    }
+}
