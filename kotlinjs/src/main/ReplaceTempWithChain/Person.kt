30ad5cdc46c6a9cd7fd3c3f3029d0b44f5804fdc
lsyiverson <lsyiverson@gmail.com>
2017-07-30 23:44:27 +0800

    [Siyang] - Replace Temp with Chain - Return self from all setter

--- kotlinjs/src/main/ReplaceTempWithChain/Person.kt
+++ kotlinjs/src/main/ReplaceTempWithChain/Person.kt
@@ -9,16 +9,19 @@ class Person {
         this.age = 0
     }
 
-    fun setName(name: String) {
+    fun setName(name: String): Person {
         this.name = name
+        return this
     }
 
-    fun setSex(sex: String) {
+    fun setSex(sex: String): Person {
         this.sex = sex
+        return this
     }
 
-    fun setAge(age: Int) {
+    fun setAge(age: Int): Person {
         this.age = age
+        return this
     }
 
     override fun toString(): String {
