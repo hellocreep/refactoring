69f47c13a633b0e5109c609057cb9b3fb27b425d
JellyLu <634725546@qq.com>
2017-08-09 23:01:26 +0800

    [Yangjie] -Pull Up Field- Remove field in subclasses.

--- java/src/main/java/com/thoughtworks/refactoring/pullUpField/SavingAccount.java
+++ java/src/main/java/com/thoughtworks/refactoring/pullUpField/SavingAccount.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.refactoring.pullUpField;
 
 public class SavingAccount extends Account {
-    private int _minimumCheckingBalance = 5;
 
     public int getMinimumCheckingBalance() {
         return _minimumCheckingBalance;
