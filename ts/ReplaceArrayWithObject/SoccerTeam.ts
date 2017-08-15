60ea725dddaa77024cb58322e89a1c211453fbd2
mrbone <sgbfire@gmail.com>
2017-08-15 23:13:08 +0800

    [chengbo] - Replcace Array With Object - remove useless array property.

--- ts/ReplaceArrayWithObject/SoccerTeam.ts
+++ ts/ReplaceArrayWithObject/SoccerTeam.ts
@@ -1,20 +1,21 @@
 class SoccerTeam {
-  private _data: string[] = new Array(3);
+  private _name: string;
+  private _wins: string;
 
   setName(name:string): void {
-    this._data[0] = name;
+    this._name = name;
   }
 
   getName(): string{
-    return this._data[0];
+    return this._name;
   }
 
   setWins(wins:string): void {
-    this._data[1] = wins; 
+    this._wins = wins; 
   }
 
   getWins(): number{
-    return parseInt(this._data[1]);
+    return parseInt(this._wins);
   }
 }
 
